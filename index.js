38b76c0006d97ce8f61c3f4a363bde389fd986ba
VivianTang11 <vivian.tang@hotmail.se>
2022-05-24 22:33:13 +0200

    First ui commit

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
-const computerChoice = () => {
-    const array = ['rock', 'paper', 'scissors']
-    return array[Math.floor(Math.random() * array.length)];
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorBtn = document.querySelector('#scissor');
+
+let playerChoice;
+let computerChoice;
+let text;
+let playerScore = 0;
+let computerScore = 0;
+const array = ['rock', 'paper', 'scissor']
+
+function result(){
+    document.getElementById('playerScore').innerHTML = playerScore
+    document.getElementById('computerScore').innerHTML = computerScore
+    document.getElementById('text').innerHTML = text
+
+    if (playerScore == 5 || computerScore == 5) {
+        console.log('gameover');
+    } 
 }
 
-const playerChoice = () => {
-    let choice = prompt('Rock paper or scissors')
-    if (choice != null){
-        return choice
-    }
-}
+function game(){
+    document.querySelectorAll('.btns').forEach((btn)=> {
+        btn.addEventListener('click', (e) => {
+            playerChoice = e.target.name
+            document.getElementById('playerChoice').setAttribute('src', `./${playerChoice}.png`)
+            computerChoice = array[Math.floor(Math.random() * array.length)]
+            document.getElementById('computerChoice').setAttribute('src', `./${computerChoice}.png`)
 
-const playRound = (playerChoice, computerChoice) => {
-    switch (true) {
-        case playerChoice === 'rock' && computerChoice === 'scissors':
-        case playerChoice === 'paper' && computerChoice === 'rock':
-        case playerChoice === 'scissors' && computerChoice === 'paper':
-            return `You won! ${playerChoice} beats ${computerChoice}`;
-        case playerChoice === 'rock' && computerChoice === 'paper':
-        case playerChoice === 'paper' && computerChoice === 'scissors':
-        case playerChoice === 'scissors' && computerChoice === 'rock':
-            return `You lose... ${computerChoice} beats ${playerChoice}`;
-        case playerChoice === 'rock' && computerChoice === 'scissors':
-        case playerChoice === 'paper' && computerChoice === 'rock':
-        case playerChoice === 'scissors' && computerChoice === 'paper':
-            return `Its a tie!`;
-    }
+            switch (true) {
+                case playerChoice === 'rock' && computerChoice === 'scissor':
+                case playerChoice === 'paper' && computerChoice === 'rock':
+                case playerChoice === 'scissor' && computerChoice === 'paper':
+                    playerScore ++
+                    text = `Player won, ${playerChoice} beats ${computerChoice}`
+                    break
+                case playerChoice === 'rock' && computerChoice === 'paper':
+                case playerChoice === 'paper' && computerChoice === 'scissor':
+                case playerChoice === 'scissor' && computerChoice === 'rock':
+                    computerScore ++
+                    text = `Computer won... ${playerChoice} loses to ${computerChoice}`
+                    break
+                case playerChoice === 'rock' && computerChoice === 'rock':
+                case playerChoice === 'paper' && computerChoice === 'paper':
+                case playerChoice === 'scissor' && computerChoice === 'scissor':
+                    text = 'Its a tie!'
+                    break
+            }
+            result()
+        })
+    })
 }
+game()
 
-const game = () => {
-    for (let i = 0; i < 5; i++) {
-        playRound()
-    }
-}
 
-console.log(playRound(playerChoice(),computerChoice()))
 
